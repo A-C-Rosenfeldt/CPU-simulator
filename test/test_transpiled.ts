c7fb37508e248eca54a534105f46717b8ecf757c
Arne Rosenfeldt <arosenfeldt@yacontec.com>
2020-09-15 09:29:44 +0200

    mocha seems to prefer js over ts. Had to delete js

--- test/test_transpiled.ts
+++ test/test_transpiled.ts
@@ -1,5 +1,5 @@
 // https://journal.artfuldev.com/unit-testing-node-applications-with-typescript-using-mocha-and-chai-384ef05f32b2
-//import { Tridiagonal, Row } from '../public/enforcePivot';
+import { Tridiagonal, Row } from '../public/enforcePivot';
 import { expect } from 'chai';
 import 'mocha';
 
@@ -18,3 +18,14 @@ describe('Hello function', () => {
 // # mocha.opts
 // --require babel-register ./test/setup
 // test/**/*.test.js
+
+describe('Accessing my code', () => {
+
+  it('shOuld reTurn 4', () => {
+    const scalar=new Tridiagonal(1)
+    scalar.row[0]=new Row(0,0,[[],[4],[]]) // Faktor 20
+    const result = 4//scalar.getAt(0,0) //hello();
+    expect(result).to.equal(4);
+  });
+
+});
