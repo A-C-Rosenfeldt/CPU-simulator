6cb731c5782e61559d634a18feebda2c116b2e04
Arne Rosenfeldt <Arne.Rosenfeldt@gmail.com>
2021-12-31 21:59:15 +0100

    tests for 2 cell field

--- .mocharc.js
+++ .mocharc.js
@@ -11,7 +11,7 @@ module.exports = {
       "js"
     ],
     "spec": [
-      "test/*.js"
+      "test/**/*.js"
     ]
   }
 };
--- test/field.ts
+++ test/field.ts
@@ -1,5 +1,5 @@
 import { Row } from '../src/enforcePivot';
-import { MapForField, exampleField, FieldToDiagonal, fieldTobeSquared, Field, bandsGapped } from '../src/fields'
+import { MapForField, exampleField, FieldToDiagonal, fieldTobeSquared, Field, bandsGapped, Contact } from '../src/fields'
 import { main } from '../src/GL';
 import { ContactedField } from '../src/fieldStatic'
 
@@ -16,7 +16,7 @@ import 'mocha';
 
 // Static field
 
-const tri=new Field(fieldTobeSquared,null)  // from TestFields.ts 
+const tri = new Field(fieldTobeSquared, null)  // from TestFields.ts 
 
 // create the laplacian: from public testFields
 // const NoSwap=new Field(bandsGapped)
@@ -67,16 +67,16 @@ tri.ToDoubleSquareMatrixSortByKnowledge_naive()
 // Neumann: eletric field strength or a case with surface charge? I feel like this should not be happening in digital circuits.
 // Cauchy: Single side. May be interesting for an antenna maybe? Not in current scope
 
- // charge density is not calculated for zero in curved space LaPlacian. Voltage is taken from contact[0]
-const metalScalar:string[]=['m'];  // curved space degenerated => voltage=0=charge
-const insulatorScalar:string[]=['i']; // curved space degenerated =>  voltage=0=charge
-const degneratedVector:string[]=['mm']; // test floodfill float , huh
-const degneratedMatrix:string[]=['mm','mm'];
+// charge density is not calculated for zero in curved space LaPlacian. Voltage is taken from contact[0]
+const metalScalar: string[] = ['m'];  // curved space degenerated => voltage=0=charge
+const insulatorScalar: string[] = ['i']; // curved space degenerated =>  voltage=0=charge
+const degneratedVector: string[] = ['mm']; // test floodfill float , huh
+const degneratedMatrix: string[] = ['mm', 'mm'];
 
 // LaPlace does indeed work here because we get 1 on the main diagonal
-const insulatorOneContact:string[]=['mi']; // one contact, one voltage, no charge
+const insulatorOneContact: string[] = ['mi']; // one contact, one voltage, no charge
 // LaPlace starts to work at 1d  ( also: border of a 2d patch )
-const insulator:string[]=['mim']; // So it seems that this would not clear the determinant. So i is curved space
+const insulator: string[] = ['mim']; // So it seems that this would not clear the determinant. So i is curved space
 // I have to determine after swapping. So swap function on translate TouchTyped ?
 // det is sum( dets) . -only one of those dets is != 0 before. Laplace adds a second
 // this is used to decide if i=contact 0  XOR  curved space
@@ -85,13 +85,13 @@ const insulator:string[]=['mim']; // So it seems that this would not clear the d
 // swap within one side would not change .. yeah but we deliberately change to the other side of the equation
 // Maybe I should just log at which line Matrix inverse fails. After all it goes from top to bottom -- left to right. Useful for degubbing
 
-const contacts2d_testcase:string[]=['0i1']; // test floodfill, degenerated
+const contacts2d_testcase: string[] = ['0i1']; // test floodfill, degenerated
 // so order: first do metal contacts and then try to add i going from highest LaPlace to lowest ( with zero, charge does not couple to voltage at all).
 
- // first case where charge and voltage are interlaced left and right
-const bulk=['iii','iii','iii']
-const metal=['mmm','mmm','mmm']
-const contacts=['m0m','iii','m1m'] // two contacts. What do with i on border? Do not set voltage .. curved space would work, after all
+// first case where charge and voltage are interlaced left and right
+const bulk = ['iii', 'iii', 'iii']
+const metal = ['mmm', 'mmm', 'mmm']
+const contacts = ['m0m', 'iii', 'm1m'] // two contacts. What do with i on border? Do not set voltage .. curved space would work, after all
 // real floodfill application
 
 
@@ -103,23 +103,105 @@ const contacts=['m0m','iii','m1m'] // two contacts. What do with i on border? Do
 
 // check voltage==0 && charge==0
 
+// low impedance wires have the numbers 0 .. 4
+const degneratedScalarContacted: string[] = ['0']; // done
+const floatTo: string[] = ['2i']
+const contactsAverage: string[] = ['0i1']; // check average in center
+const contacts2d: string[] = ['0ii', 'ii1']; // should all float up to the one given potential
+const contactsSquare: string[] = ['0ii', 'iii', 'ii2']; // // check average in center
+
+const contactsAverageV: string[] = ['0','i','2']; // check average in center
+describe('i0', () => {
+	it('should all float to the refernece',()=>{
+		const NoSwap = new Field(floatTo)
+		const [v,m] = NoSwap.ShapeToSparseMatrix();
+		const rn = NoSwap.fieldInVarFloats[0][1].RunningNumberOfJaggedArray;
+		expect(rn).to.equal(0)
+		expect(m.getAt(rn,rn)).to.equal(1)  //  1/1 = 1
+	})
+	it('should all float to the refernece mirror',()=>{
+		const NoSwap = new Field(['i2'])
+		const [v,m] = NoSwap.ShapeToSparseMatrix();
+		const rn = NoSwap.fieldInVarFloats[0][1].RunningNumberOfJaggedArray;
+		expect(rn).to.equal(0)
+		expect(m.getAt(rn,rn)).to.equal(1)  //  1/1 = 1
+	})	
+	it('should all float to the refernece transpose',()=>{
+		const NoSwap = new Field(['2','i'])
+		const [v,m] = NoSwap.ShapeToSparseMatrix();
+		const rn = NoSwap.fieldInVarFloats[1][0].RunningNumberOfJaggedArray;
+		expect(rn).to.equal(0)
+		expect(m.getAt(rn,rn)).to.equal(1)  //  1/1 = 1
+	})	
+})
+
 describe('sort columns all on one side', () => {
 	it('should return hello world 2020-11-19 18:35', () => {
-  
-	const checkContact=new ContactedField(metalScalar)
 
-	// 2020 Version checkContact.lowImpedanceContacts[0].voltage=1
-	checkContact.fieldInVarFloats[0][0].Potential=1  // 2021 Version
+		const checkContact = new ContactedField(metalScalar)
 
-	const m=checkContact.ToDoubleSquareMatrixSortByKnowledge_naive()
-	// check
-	//m.
+		// 2020 Version checkContact.lowImpedanceContacts[0].voltage=1
+		checkContact.fieldInVarFloats[0][0].Potential = 1  // 2021 Version
 
-    //const scalar=new Tridiagonal(1)
-    //scalar.row[0]=new Row(0,0,[[],[4],[]]) // Faktor 20
-    const result = 4//scalar.getAt(0,0) //hello();
-    expect(result).to.equal(4);
-  });
+		const m = checkContact.ToDoubleSquareMatrixSortByKnowledge_naive()
+		// todo
+		const result = 4//scalar.getAt(0,0) //hello();
+		expect(result).to.equal(4);
+	});
+
+
+	it('2021-12-18 metal', () => {
+		const NoSwap = new Field(degneratedScalarContacted)
+		// 2020 Version checkContact.lowImpedanceContacts[0].voltage=1
+		// 2022, now I have CAPS for contacts and numerals for fixed potential.  Was: checkContact.fieldInVarFloats[0][0].Potential = 1; // 2021 Version
+		const m = NoSwap.ShapeToSparseMatrix();
+		expect(NoSwap.fieldInVarFloats[0][0].RunningNumberOfJaggedArray).to.not.exist // field has only one cell which is grounden => nothing to solve
+		// check .. ah JS without TS intellisense is a pain to write tests :-()
+		//(0, chai_1.expect)(m.row[0]).to.exist;
+
+		//NoSwap.row ;
+		console.log(" use dynamic to access data ") // no typeScript anymore :-()
+		//const scalar=new Tridiagonal(1)
+		//scalar.row[0]=new Row(0,0,[[],[4],[]]) // Faktor 20
+
+	});
+
+	it('2021-12-18 insulator', () => {
+		const contacts = Field.CreateContactBareMetal()
+		Field.SetContact(contacts, 'M', new Contact())
+		const NoSwap = new Field(insulatorScalar, contacts)
+
+		// the method should be able to just create the differential equation without the boundary ( closed space like on a sphere or torus). This matrix can not be inverted
+		const m = NoSwap.ShapeToSparseMatrix();
+		expect(NoSwap.fieldInVarFloats[0][0].RunningNumberOfJaggedArray).to.equal(0)
+		// check
+		//m.
+		//const scalar=new Tridiagonal(1)
+		//scalar.row[0]=new Row(0,0,[[],[4],[]]) // Faktor 20
+		const result = 4; //scalar.getAt(0,0) //hello();
+		expect(result).to.equal(4);
+	});
+
+
+	it('2021-12-18 metal which expects a wire', () => {
+
+		const contacts = Field.CreateContactBareMetal()
+		Field.SetContact(contacts, 'M', new Contact())
+
+		const NoSwap = new Field(['M'], contacts)
+		// 2020 Version checkContact.lowImpedanceContacts[0].voltage=1
+		// 2022, now I have CAPS for contacts and numerals for fixed potential.  Was: 
+		NoSwap.fieldInVarFloats[0][0].Potential = 1; // 2021 Version
+		//NoSwap.contacts.contacts.  I think 
+		const m = NoSwap.ShapeToSparseMatrix();
+		// check
+		//m.
+		//const scalar=new Tridiagonal(1)
+		//scalar.row[0]=new Row(0,0,[[],[4],[]]) // Faktor 20
+		const result = 4; //scalar.getAt(0,0) //hello();
+		expect(result).to.equal(4);
+	});
+
 
 });
 
@@ -127,16 +209,14 @@ describe('sort columns all on one side', () => {
 
 
 
-// low impedance wires have the numbers 0 .. 4
-const degneratedScalarContacted:string[]=['0'];
-const contactedSpreads:string[]=['0m'];
-const contactedMatrix:string[]=['0m','mm'];
+
+
+// wire mechanism is not basic --  as far as I have seen with the examples in University. Solve for Q is something different
+const contactedSpreads: string[] = ['0m'];
+const contactedMatrix: string[] = ['0m', 'mm'];
 
 // check voltage==-5 && charge==0 .. Better define an offset? So 0 == -5  and  1 == -3.3
 
 
 // FinFet has contacts, so try the cases above with contact to anything != GND
 
-const contactsAverage:string[]=['0i1']; // check average in center
-const contacts2d:string[]=['0ii','ii1'];
-const contactsSquare:string[]=['0ii','iii','ii1']; // // check average in center
