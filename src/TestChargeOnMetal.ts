e0a19c0b1ad7ea8ed841c0e66640ba460b45a066
Arne Rosenfeldt <Arne.Rosenfeldt@gmail.com>
2022-04-23 13:12:44 +0200

    demo of test for 3 symmetries

--- src/TestChargeOnMetal.ts
+++ src/TestChargeOnMetal.ts
@@ -26,7 +26,7 @@ const imageM: SimpleImage[] = []
 	//imageM.push(m.PrintGl());
 	m.inverseRectangular() // in place. Still wonder if I should provide a immutable version
 	//		imageM.push(m.PrintGl());
-	setContactVoltages(Swap, v, [ 2, 4])
+	setContactVoltages(Swap, v, [2, 4])
 	const M2 = m.split() // uses the -1 from above.  //new Tridiagonal(0) // split does not work in place because it may need space at the seam
 	// Maybe hide/encapsulate the rows? M2.row = m.row.map(r => r.split(1, m.row.length)) // this is ugly internal stuff. I guess I need in place before I can get new features.
 	const potential = M2.MatrixProduct(v) // No charge yet .. so all semiconductor entries are 0 . I sure need to test that before I add carriers ( tube .. before doping )
@@ -42,81 +42,95 @@ const imageM: SimpleImage[] = []
 
 {
 	const images: SimpleImage[] = []
-	
-{
-	// taken from test/fields.ts .. Replaced literal voltage digits by capital letters
-	const contacts2d: string[] = 
-	['BBsss',
-	'BBsss',
-	'sssss',
-	'sssAA',
-		'sssAA'];
-
-
-	const Swap = new Field(contacts2d)
-	const [v, m] = Swap.ShapeToSparseMatrix();
-
-	var imageGlM = m.PrintGl()
-	//imageM.push( imageGlM) //main('FieldGl0_field',imageGl) 
-	m.AugmentMatrix_with_Unity(-1)
-	Swap.GroupByKnowledge(m) // Optional laast parameter: I don't think I drop columns here. Was all in vector and ShapeToSparse Matrix
-	// too big imageM.push(m.PrintGl());
-	m.inverseRectangular() // in place. Still wonder if I should provide a immutable version
-	//		imageM.push(m.PrintGl());
-	setContactVoltages(Swap, v, [ 2, 4])
-	const M2 = m.split() // uses the -1 from above.  //new Tridiagonal(0) // split does not work in place because it may need space at the seam
-	// Maybe hide/encapsulate the rows? M2.row = m.row.map(r => r.split(1, m.row.length)) // this is ugly internal stuff. I guess I need in place before I can get new features.
-	const potential = M2.MatrixProduct(v) // No charge yet .. so all semiconductor entries are 0 . I sure need to test that before I add carriers ( tube .. before doping )
-	Swap.pullInSemiconductorVoltage(potential) // opposite of groupByKnowledge
-
-
-	var imageGl = Swap.PrintGl()
-	images.push(imageGl) //main('FieldGl0_field',imageGl) 
-
-	// too big  main('metal_charge_matrix', imageM)
-	//main('surfac_bulk', images)
-}
-
-{
-//	const images: SimpleImage[] = []
-	
 
 	// taken from test/fields.ts .. Replaced literal voltage digits by capital letters
-	const contacts2d: string[] = 
-	['BBBssss',
-	'BBBssss',
-	'BBBssss',	
-	'sssssss',
-	'ssssAAA',
-	'ssssAAA',
-	'ssssAAA'];
+	{
+		var contacts2d: string[] =
+			['BBsss',
+				'BBsss',
+				'sssss',
+				'sssAA',
+				'sssAA'];
+
+		var voltages = [2, 4];
+
+		for (var variations = 0; variations < 8; ) {
 
 
-	const Swap = new Field(contacts2d)
-	const [v, m] = Swap.ShapeToSparseMatrix();
 
-	//var imageGlM = m.PrintGl()
-	//imageM.push( imageGlM) //main('FieldGl0_field',imageGl) 
-	m.AugmentMatrix_with_Unity(-1)
-	Swap.GroupByKnowledge(m) // Optional laast parameter: I don't think I drop columns here. Was all in vector and ShapeToSparse Matrix
-	imageM.push(m.PrintGl()); // too big
-	m.inverseRectangular() // in place. Still wonder if I should provide a immutable version
-
-	setContactVoltages(Swap, v, [ 2, 4])
-	imageM.push(m.PrintGl());
-	const M2 = m.split() // uses the -1 from above.  //new Tridiagonal(0) // split does not work in place because it may need space at the seam
-	imageM.push(M2.PrintGl());
-	
-	const potential = M2.MatrixProduct(v) // No charge yet .. so all semiconductor entries are 0 . I sure need to test that before I add carriers ( tube .. before doping )
-	Swap.pullInSemiconductorVoltage(potential) // opposite of groupByKnowledge
+			var Swap = new Field(contacts2d)
+			var [v, m] = Swap.ShapeToSparseMatrix();
 
 
-	var imageGl = Swap.PrintGl()
-	images.push(imageGl) //main('FieldGl0_field',imageGl) 
+			//imageM.push( imageGlM) //main('FieldGl0_field',imageGl) 
+			m.AugmentMatrix_with_Unity(-1)
+			Swap.GroupByKnowledge(m) // Optional laast parameter: I don't think I drop columns here. Was all in vector and ShapeToSparse Matrix
+			// too big imageM.push(m.PrintGl());
+			m.inverseRectangular() // in place. Still wonder if I should provide a immutable version
 
-	// too big  main('metal_charge_matrix', imageM)
-	main('surfac_bulk', images)
-}
+			//		imageM.push(m.PrintGl());
+			setContactVoltages(Swap, v, voltages)
+			const M2 = m.split() // uses the -1 from above.  //new Tridiagonal(0) // split does not work in place because it may need space at the seam
+			// Maybe hide/encapsulate the rows? M2.row = m.row.map(r => r.split(1, m.row.length)) // this is ugly internal stuff. I guess I need in place before I can get new features.
+			const potential = M2.MatrixProduct(v) // No charge yet .. so all semiconductor entries are 0 . I sure need to test that before I add carriers ( tube .. before doping )
+			Swap.pullInSemiconductorVoltage(potential) // opposite of groupByKnowledge
 
-main('metal_charge_matrix', imageM)
+			images.push(Swap.PrintGl())
+
+			if ((++variations & 7) == 4) contacts2d=contacts2d.map(s => {
+				// strings are immutable in JS. But arrays are not. So we are forced to use Join.
+				const a = s.split('')
+				a.reverse()
+				return a.join("")
+			})
+			if ((variations & 3) == 2) contacts2d.reverse()
+			if ((variations & 1) == 1) voltages.reverse()
+		}
+	}
+
+	//main('metal_charge', imageM)
+	// }
+
+	// {
+	// 	const images: SimpleImage[] = []
+
+	{
+		// taken from test/fields.ts .. Replaced literal voltage digits by capital letters
+		const contacts2d: string[] =
+			['BBBssss',
+				'BBBssss',
+				'BBBssss',
+				'sssssss',
+				'ssssAAA',
+				'ssssAAA',
+				'ssssAAA'];
+
+
+		const Swap = new Field(contacts2d)
+		const [v, m] = Swap.ShapeToSparseMatrix();
+
+		//var imageGlM = m.PrintGl()
+		//imageM.push( imageGlM) //main('FieldGl0_field',imageGl) 
+		m.AugmentMatrix_with_Unity(-1)
+		Swap.GroupByKnowledge(m) // Optional laast parameter: I don't think I drop columns here. Was all in vector and ShapeToSparse Matrix
+		imageM.push(m.PrintGl()); // too big
+		m.inverseRectangular() // in place. Still wonder if I should provide a immutable version
+
+		setContactVoltages(Swap, v, [2, 4])
+		imageM.push(m.PrintGl());
+		const M2 = m.split() // uses the -1 from above.  //new Tridiagonal(0) // split does not work in place because it may need space at the seam
+		imageM.push(M2.PrintGl());
+
+		const potential = M2.MatrixProduct(v) // No charge yet .. so all semiconductor entries are 0 . I sure need to test that before I add carriers ( tube .. before doping )
+		Swap.pullInSemiconductorVoltage(potential) // opposite of groupByKnowledge
+
+
+		var imageGl = Swap.PrintGl()
+		images.push(imageGl) //main('FieldGl0_field',imageGl) 
+
+		// too big  main('metal_charge_matrix', imageM)
+		main('surfac_bulk', images)
+	}
+
+	main('metal_charge_matrix', imageM)
 }
