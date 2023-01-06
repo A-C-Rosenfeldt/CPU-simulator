809695c172bd15f2eff0d767033c7108f124c492
Arne Rosenfeldt <Arne.Rosenfeldt@gmail.com>
2023-01-06 12:08:44 +0100

    2d mesh / lattice based Navie Stokes

--- src/NavierStokes.ts
+++ src/NavierStokes.ts
@@ -1,3 +1,4 @@
+
 /*
 {\displaystyle {\frac {\partial {\vec {m}}}{\partial t}}+\nabla \cdot ({\vec {v}}\otimes {\vec {m}})=-\nabla p+\mu \Delta {\vec {v}}+{\frac {\mu }{3}}\nabla (\nabla \cdot {\vec {v}})+{\vec {f}}.}
 
@@ -13,7 +14,41 @@ zustandslgleichung
 	aber Ladungsträger
 
 
+1d fall -> bild malen . Flow through a tube
+sound waves. Linear approximation around a base pressure. Shock waves? I want to show the start with this trick where swap is replaced by add and sub.
+diffusion ( grid effect ) is okay for a semiconductor ( i abondoned tube due to: 
+	viscosity is important to me. Needs 2d?
+	)
+
 */
+
+Print(): ImageData { //ToPicture   print=text vs picture?
+    const iD = new ImageData(this.maxStringLenght, this.touchTypedDescription.length)
+    // RGBA. This flat data structure resists all functional code
+    // ~screen
+    for (let pointer = 0; pointer < iD.data.length; pointer += 4) {
+      iD.data.set([0, 25, 0, 255], pointer) // ~dark green 
+    }
+
+function show(){
+	requestAnimationFrame(render);
+
+	var id = null;
+	function myMove() {
+	  var elem = document.getElementById("t_animated");
+	  var pos = 0;
+	  clearInterval(id);
+	  id = setInterval(frame, 10);
+	  function frame() {
+		if (pos == 350) {
+		  clearInterval(id);
+		} else {
+			ctx.drawImage(this, 0, 0);
+		}
+	  }
+	}	
+
+}
 
 var v:number[][][]
 var x:number,y:number;
